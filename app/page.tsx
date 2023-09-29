154f7ea50366b74324e27aad839c1f777a61f79d
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-09-29 12:18:26 +0530

    feat: design ui for the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [show, onShow] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#9EDDFF] text-[#2E4374]">
-      <div className="bg-white">Github Explorer</div>
+    <main className="flex flex-col h-screen items-center justify-between py-24 bg-[#F0F0F0] text-[#2E4374] w-full">
+      <div className="bg-[#9EDDFF] w-1/3 flex flex-col items-center justify-between rounded-lg h-1/3 py-8">
+        <h1>Search for profile!</h1>
+        {/* <form> */}
+        <input
+          type="text"
+          placeholder="Type github username"
+          className="border-2 border-[#2E4374] w-5/6 placeholder:px-2"
+        ></input>
+        <button
+          className="border-2 border-[#2E4374] w-1/6 bg-white duration-300 hover:scale-110"
+          onClick={() => onShow(true)}
+        >
+          Search
+        </button>
+        {/* </form> */}
+      </div>
+
+      {show && <div>No Results</div>}
     </main>
   );
 }
