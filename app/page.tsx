e7f88960d35337289e0c0637d23b91656926a51b
Aayushi Agarwal <98597479+Aayushi-Agrwal@users.noreply.github.com>
2024-01-14 18:39:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
 
 export function requestUserRepos(username: string) {
@@ -64,37 +65,61 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col h-screen items-center justify-between py-24 bg-[#F0F0F0] text-[#2E4374] w-full">
-      <div className="bg-[#9EDDFF] w-1/3 flex flex-col items-center justify-between rounded-lg h-1/3 py-8">
-        <h1>Search for GitHub Profile!</h1>
-        <input
-          type="text"
-          placeholder="Type GitHub username"
-          className="border-2 border-[#2E4374] w-5/6 placeholder:px-2"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <button
-          className="border-2 border-[#2E4374] w-1/6 bg-white duration-300 hover:scale-110"
-          onClick={handleSearch}
-          type="submit"
-        >
-          Search
-        </button>
-        {error && <p>{error}</p>}
+    <main className="flex flex-col h-screen items-center justify-between bg-white w-full">
+      <Image
+        src="/bg.svg"
+        alt=""
+        width={700}
+        height={700}
+        className="opacity-50 fixed z-10"
+      />
+      <div className="absolute z-20 flex flex-col items-center justify-between h-full w-full">
+        <div className="w-1/2 flex flex-col items-center justify-between rounded-lg h-1/3 mt-32 z-20">
+          <h1 className="text-lg font-semibold">
+            Simplifying github account searches
+          </h1>
+          <p className="text-sm text-center w-5/6">
+            Easier to discover profiles, repositories swiftly. Navigate the
+            GitHub landscape effortlessly with our intuitive and efficient
+            search tool.
+            {/* for GitHub Profile! bg-[#9EDDFF] text-[#2E4374] */}
+          </p>
+          <div className="w-full shadow-xl flex items-center justify-center gap-4 bg-white rounded-sm h-24">
+            <input
+              type="text"
+              placeholder="GitHub username"
+              className="border border-slate-100 rounded-md shadow-md w-2/3 py-2 h-12 placeholder:px-2"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <button
+              className="bg-[#9EDDFF] w-1/6 h-12 duration-300 hover:scale-110"
+              onClick={handleSearch}
+              type="submit"
+            >
+              Search
+            </button>
+          </div>
+          {error && <p>{error}</p>}
+        </div>
+
+        {username && (
+          <div className="w-1/2 shadow-xl flex items-center justify-center gap-4 bg-white rounded-sm h-24 mb-32">
+            <h2>Number of Repositories: {repos.length}</h2>
+            <h2>Number of Followers: {followers.length}</h2>
+
+            <Link
+              href={`https://github.com/${username}`}
+              className="border-2 p-2 border-[#2E4374] hover:bg-white "
+              target="_blank"
+            >
+              Github profile
+            </Link>
+          </div>
+        )}
       </div>
-      <div>
-        <h2>Number of Repositories: {repos.length}</h2>
-        <h2>Number of Followers: {followers.length}</h2>
-      </div>
-      <Link
-        href={`https://github.com/${username}`}
-        className="border-2 p-2 border-[#2E4374] hover:bg-white"
-        target="_blank"
-      >
-        Github profile
-      </Link>
     </main>
   );
 }
+
